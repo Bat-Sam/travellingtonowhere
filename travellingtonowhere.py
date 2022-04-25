09e43d89a626d2d4b66a707be1ceffa2ade94fcd
Bat-Sam <54631899+Bat-Sam@users.noreply.github.com>
2022-04-25 19:13:58 +0600

    Add files via upload

---
+++ travellingtonowhere.py
@@ -0,0 +1,24 @@
+
+#Opening
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0", str(name))
+trip_planner_welcome("Triple H")
+
+#Estimated Time
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+estimate = estimated_time_rounded(16.55)
+
+#Setup
+def destination_setup(origin,destination,estimated_time,mode_of_transport = "Car"):
+   print("Your trip starts off in" ,  origin)
+   print("And you are traveling to" , destination)
+   print("You will be traveling by" , 
+   mode_of_transport)
+   print("It will take approximately" ,  str(estimated_time) ,  "hours")
+
+destination_setup("Orlando","New York", estimate)
+
+
+
